bfe1d9812295e769598b9c280a6463c241b0391d
Vic <chinwei@indeed.com>
2016-07-20 20:30:47 -0500

    action created

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: the Good parts' },
+    { title: 'Harry Potter' },
+    { title: ' The dark tower' },
+    { title: ' the elogquent ruby'}
+  ];
+}
